5c2909975700d92b0664296a3ad099ac24f44ed6
Christoph89 <christoph.schaunig@live.com>
2017-10-09 14:56:51 +0200

    seo
    concat external js
    minify js

--- gulp/scripts.js
+++ gulp/scripts.js
@@ -1,12 +1,24 @@
+var concat=require("gulp-concat");
+var uglify=require("gulp-uglify");
+
 // copies all js files
 function run($) 
 {
-  return $.copy([
+  // concat external js
+  var stream=$.src([
     "%srcvendor/jquery/*.js",
     "%srcvendor/modernizr/modernizr.js",
     "%srcvendor/skel/skel.js",
     "%srcvendor/linq/linq.js",
-  ], "%dstjs");
+  ])
+  .pipe(concat("external.js"));
+
+  // minify 
+  if ($.cfg.min)
+    stream=stream.pipe(uglify({}));
+
+  // set dest
+  return stream.dest("%dstjs");
 }
 
 module.exports={
